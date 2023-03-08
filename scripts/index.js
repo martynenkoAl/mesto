72699186656408c477176bd9f0cf0e01cfd319e6
Alina Martynenko <martynenko.av.94@mail.ru>
2023-03-08 13:44:56 +1000

    fix errors after the first check

--- scripts/index.js
+++ scripts/index.js
@@ -2,37 +2,45 @@ const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close');
 const popupOpenButtonElement = document.querySelector('.profile__edit-btn');
 let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__text_name');
-let jobInput = document.querySelector('.popup__text_job');
+let nameInput = document.querySelector('.popup__text_type_name');
+let jobInput = document.querySelector('.popup__text_type_job');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
 
-nameInput.value = profileName.textContent;
-jobInput.value = profileJob.textContent;
 
+// открываем попап
 const openPopup = function () {
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
   popupElement.classList.add('popup_opened');
 };
 
+// закрываем попап
 const closePopup = function () {
   popupElement.classList.remove('popup_opened');
 };
 
+/*
 const closePopupByClickOverlay = function(event) {
   if (event.target !== event.currentTarget) {
     return;
   }
   closePopup();
 };
+*/
 
-popupOpenButtonElement.addEventListener('click', openPopup);
-popupCloseButtonElement.addEventListener('click', closePopup);
-popupElement.addEventListener('click', closePopupByClickOverlay);
 
+// отправляем данные формы
 function handleFormSubmit (evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
+  closePopup();
 }
 
 formElement.addEventListener('submit', handleFormSubmit);
+popupOpenButtonElement.addEventListener('click', openPopup);
+popupCloseButtonElement.addEventListener('click', closePopup);
+/*
+popupElement.addEventListener('click', closePopupByClickOverlay);
+*/
